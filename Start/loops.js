7a10f59e633f05a9510cba4410bb80c629dd6a8a
Kuntal Banerjee <94304703+Kuntal2003@users.noreply.github.com>
2025-09-08 12:15:30 +0000

    loops added

---
+++ Start/loops.js
@@ -0,0 +1,58 @@
+// const arr = [1,2,3,4,5,6,7,8,9,10];
+// for loops
+// for(let i = 0;i<10;i++){
+//     console.log(arr[i]*arr[i]);
+// }
+
+const { use, useState } = require("react");
+
+// while loops
+// let i = 0;
+// while(i<arr.length){
+//     console.log(arr[i]*arr[i]);
+//     i++;
+// }
+
+// do while loops
+// let i = 0;
+// do{
+//     console.log(arr[i]*arr[i]);
+//     i++;
+// }while(i<arr.length);
+
+// for-Of loop --> provide array element inside loop
+// for(i of arr){
+//     console.log(i*i);
+// }
+
+// for-In loop --> provide index number
+// for(i in arr){
+//     console.log(arr[i]*arr[i]);
+// }
+
+const users = [
+  { id: 1, name: "Alice", age: 25, isActive: true },
+  { id: 2, name: "Bob", age: 30, isActive: false },
+  { id: 3, name: "Charlie", age: 22, isActive: true },
+  { id: 4, name: "David", age: 28, isActive: false },
+  { id: 5, name: "Eva", age: 35, isActive: true }
+];
+// for each loop
+// users.forEach((item)=>{
+//     console.log(item.name);
+// })
+
+// Map method
+// const newArr = users.map((items) => items.age);
+// console.log(newArr);
+
+// fiter method
+// const newArr = users.filter((items)=>{
+//     return items.isActive == true;
+// })
+// const newArr = users.filter(itmes => itmes.isActive);
+// console.log(newArr);
+
+// reduce method
+const returnedNewVar = users.reduce((newVer , items)=> (items.age + newVer),0);
+console.log(returnedNewVar);
